86e20308a27b3462cf1beeb77dd824152f2b4087
Jared <jster004@gmail.com>
2024-04-04 12:00:34 -0500

    Continued work on Player Controller.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,8 +10,12 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     //The maximum speed of Houston
     public float maxSpeed;
+    //The jumping strength of Houston
+    public float jumpPower;
+    public float reactionPower;
 
     private float _movementForce;
+    private Vector2 _reactiveForce;
     private Rigidbody2D _rb;
     private Animator _an;
 
@@ -38,6 +42,12 @@ public class PlayerController : MonoBehaviour
         else
         {
             _an.SetBool("directionPressed", false);
+
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            _rb.AddForce(transform.up * (jumpPower * 1000));
         }
     }
 
@@ -45,6 +55,10 @@ public class PlayerController : MonoBehaviour
     {
         //We need to calculate the amount of force needed to accelerate Houston to the desired maximum speed
         //F = m * a, a = v0 - vf / t
-        _movementForce = _rb.mass * ((maxSpeed - _rb.velocity.x) / (Time.fixedDeltaTime * 1000));
+        _movementForce = _rb.mass * ((maxSpeed - Mathf.Abs(_rb.velocity.x)) / (Time.fixedDeltaTime * 100));
+        print(_movementForce);
+
+        //This calculates the reactive force to slow Houston down for tighter controls.
+        _reactiveForce = _rb.velocity.normalized * -1 * reactionPower;
     }
 }
