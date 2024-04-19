679ac88c9035a9ad3361c8ae9b26af8467eb2e7a
Jared <jster004@gmail.com>
2024-04-19 17:42:49 -0500

    Implemented Jump Buffer.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,12 @@ public class PlayerController : MonoBehaviour
     [Range(1,2)]
     public float reactionPower;
 
+    [Header("Jumping")] 
+    public float jumpPower;
+    public bool onGround;
+    public float jumpBufferDuration;
+    private float _jumpBufferTimer;
+
     private Rigidbody2D _rb;
     private Creature _c;
     private Animator _animator;
@@ -25,10 +31,15 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         MoveCharacter();
+        
+        UpdateTimers();
     }
 
     private void Update()
     {
+        Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        print(inputVector);
+        
         //Change the target movement speed based on the input
         targetMovementSpeed = movementSpeed * Input.GetAxis("Horizontal");
         
@@ -36,6 +47,25 @@ public class PlayerController : MonoBehaviour
         if (Input.GetAxis("Horizontal") != 0)
         {
             transform.localScale = new Vector3(Mathf.Sign(targetMovementSpeed), 1, 1);
+        }
+        
+        
+
+        //If the key is pressed to jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            //Make sure we are on the ground before jumping
+            if (Physics2D.OverlapBox(transform.position, new Vector2(0.45f,1), transform.position.z, 1<<7))
+            {
+                print("j");
+                //Then jump
+                Jump();
+            }
+            //If we are are not on the ground, buffer the jump
+            else
+            {
+                _jumpBufferTimer = jumpBufferDuration;
+            }
         }
     }
 
@@ -64,5 +94,41 @@ public class PlayerController : MonoBehaviour
             //Not Moving Animation
             _animator.SetBool("directionPressed", false);
         }
+    }
+
+    public void Jump()
+    {
+        //When we jump, we want to apply an impulse in the opposite direction of gravity
+        _rb.AddForce(jumpPower * transform.up, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        //If we land on the ground and the collider that hit the ground is the foot collider
+        if (other.gameObject.CompareTag("Ground") && other.otherCollider == gameObject.GetComponents<BoxCollider2D>()[1])
+        {
+            onGround = true;
+            
+            //If the jump is buffered, we want to follow through
+            if (_jumpBufferTimer > 0)
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        //If we are leaving ground, we are no longer onGround
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            onGround = false;
+        }
+    }
+    
+    //This function handles updating important timers
+    void UpdateTimers()
+    {
+        _jumpBufferTimer -= Time.fixedDeltaTime;
     }
 }
