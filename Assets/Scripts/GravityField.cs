2e8822f5629d2b712c6679fa34c87f710435e353
Jared <jster004@gmail.com>
2024-05-16 13:22:41 -0500

    Implemented Demo Scene

--- Assets/Scripts/GravityField.cs
+++ Assets/Scripts/GravityField.cs
@@ -10,12 +10,22 @@ public class GravityField : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //UpdateGravity(other);
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        UpdateGravity(other);
+    }
+
+    void UpdateGravity(Collider2D other)
+    {
         if (other == null) return;
-        //If the other object is effected by gravity
-        if (other.gameObject.GetComponent<GravityObject>() != null)
-        {
-            //Set the direction of the gravity to that object
-            other.gameObject.GetComponent<GravityObject>().gravityDirection = fieldDirection;
-        }
+                //If the other object is effected by gravity
+                if (other.gameObject.GetComponent<GravityObject>() != null)
+                {
+                    //Set the direction of the gravity to that object
+                    other.gameObject.GetComponent<GravityObject>().gravityDirection = fieldDirection;
+                }
     }
 }
